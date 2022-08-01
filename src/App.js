4c991b042ecf541ffc5ea7d70c5a54a79d64a4b0
Ametudiore <ametudiore@outlook.com>
2022-08-01 17:44:07 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+//import logo from './logo.svg';
+import NavbarComponent from './navbarComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CarouselComponent from './carouselComponent';
+import HomeComponent from './home';
+import FooterComponent from './footerComponent';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <NavbarComponent/>
+      </header>
+      <CarouselComponent/>
+      <HomeComponent/>
+      <FooterComponent/>
+
+    </div>
+  );
+}
+
+export default App;
